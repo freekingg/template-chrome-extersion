981380def204f644542320f8c3a5486f01879932
king king <kingking@kingdeMacBook-Pro.local>
2020-12-07 09:42:18 +0400

    🎉 first-commit

---
+++ src/options/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="chrome-extension-template-options">
+    <h1>欢迎使用</h1>
+    <h1>欢迎使用</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+.chrome-extension-template-newtab {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
